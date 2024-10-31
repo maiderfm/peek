23eeead8fbfa4536077f17820b8534bd62caaa0f
Maider <maiderupvehu@gmail.com>
2024-10-31 12:57:58 +0100

    Added conditionals

--- peek.sh
+++ peek.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
-head -n $2 $1
-echo "..."
-tail -n $2 $1
+if [[ -z $2 ]];
+then
+	n_lines=3;
+else
+	n_lines=$2;
+fi;
+
+text_lines=$(cat "$1" | wc -l);
+
+if [[ $text_lines -le 2*$n_lines ]];
+then
+	cat "$1"
+else
+	echo "Warning: Number of lines in file is greater than twice the requested number of lines";
+	head -n $n_lines "$1";
+	echo "...";
+	tail -n $n_lines "$1";
+fi;
+
