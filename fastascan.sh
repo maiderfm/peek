0c8b6e70f3b4ac6fa42c418785afb3e3edce7d69
Maider <maiderupvehu@gmail.com>
2024-11-19 18:56:31 +0100

    v3

--- fastascan.sh
+++ fastascan.sh
@@ -3,9 +3,11 @@
 #$sh fastascan.sh folder N
 if [[ -d $1 ]]; #1. the folder X where to search files (default: current folder); 
 then
-n_files=$(find $1 -type f -name "*.fa" -or -name ".fasta" | wc -l );
+fastafiles=$(find $1 -type f -or -type l -name "*.fa" -or -name "*.fasta");
+n_files=$(find $1 -type f -or -type l -name "*.fa" -or -name "*.fasta" | wc -l );
 else
-n_files=$(find . -type f -name "*.fa" -or -name ".fasta" | wc -l);
+fastafiles=$(find . -type f -or -type l -name "*.fa" -or -name "*.fasta");
+n_files=$(find . -type f -or -type l -name "*.fa" -or -name "*.fasta" | wc -l );
 fi;
 
 #2. a number of lines, here called N (default: 0)
@@ -19,32 +21,47 @@ fi
 echo Searching for fasta files in $1
 echo Number of lines specified $N
 
-echo $n_lines
 #The report should include this information:
 	if [[ $n_files -gt 1 ]];#how many such files there are	
 	then 
-		echo There are $n_files;
-	ifelse [[ $n_files -eq 1]];
+		echo There are $n_files fasta files;
+	elif [[ $n_files -eq 1 ]];
+	then 
 		echo There is 1 fasta files; 
 	else
 		echo There are no fasta files;
 	fi;
 	
-for i in $fastafiles;
-do
-
-#how many unique fasta IDs (i.e. the first words of fasta headers) they contain in total 
-	grep ">" $i | uniq
-	 
 #for each file:
-filename=$(echo $i)
-symlnk=$(if [[ $i -h ]]; then echo "YES"; else echo "NO")
-numberseq=$(grep -c ">" $i )
-seqlength=$(for f in $(grep -v ">" $i); do wc -c; done)
-echo File name: $filename Symlink: $symlnk Number of sequences: $numberseq Sequence length: $seqlength
+for i in $(echo $fastafiles);
+do
+	filename=$(echo $i)
+	symlnk=$(if [[ -h $i ]]; then echo "YES"; else echo "NO"; fi)
+	numberseq=$(grep -c ">" $i )
+	seqlength=$(grep -v ">" $i | grep -vw "-" | wc -c)  #gaps "-", spaces, newline characters should not be counted
+
 #	print a nice header including filename; and:
-#	whether the file is a symlink or not
-#	how many sequences there are inside
-#	the total sequence length in each file, i.e. the total number of amino acids or nucleotides of all sequences in the file. NOTE: gaps "-", spaces, newline characters should not be counted
-#Extra points: determine if the file is nucleotide or amino acids based on their content, and print a label to indicate this in this header
+echo File name: $filename Symlink: $symlnk 
+echo Number of sequences: $numberseq Sequence length: $seqlength
+	#	whether the file is a symlink or not
+	#	how many sequences there are inside
+	#	the total sequence length in each file, i.e. the total number of amino acids or nucleotides of all sequences in the file. NOTE: gaps "-", spaces, newline characters should not be counted
+	#Extra points: determine if the file is nucleotide or amino acids based on their content, and print a label to indicate this in this header
+
 #next, if the file has 2N lines or fewer, then display its full content; if not, show the first N lines, then "...", then the last N lines. If N is 0, skip this step.
+n_lines=$(cat $i | wc -l)
+if [[ n_lines -le $(( 2*$N )) ]];
+then 
+cat $i;
+elif [[ n_lines -eq 0 ]]
+then
+	continue
+else
+head -n $N $i
+echo "..."
+tail -n $N $i
+fi;
+
+done
+#next, if the file has 2N lines or fewer, then display its full content; if not, show the first N lines, then "...", then the last N lines. If N is 0, skip this step.
+
